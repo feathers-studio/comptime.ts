91a0030d9fd631ed3e6d5b4b1e90fe15b83efedc
Muthu Kumar <muthukumar@thefeathers.in>
2025-05-13 19:57:48 +0530

    chore: improvement to build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -2,14 +2,18 @@
 
 set -euo pipefail
 
-echo '<!DOCTYPE html><html lang="en">' > docs/index.html
+printf '<!DOCTYPE html>\n<html lang="en">\n' > docs/index.html
 cat docs/head.html >> docs/index.html
+printf '<body>\n<main>\n' >> docs/index.html
 npx -y marked --gfm < README.md | sed 's/ERRORS\.md/\.\/errors/' >> docs/index.html
+printf '</main>\n' >> docs/index.html
 cat docs/tail.html >> docs/index.html
-echo '</html>' >> docs/index.html
+printf '</body>\n</html>\n' >> docs/index.html
 
-echo '<!DOCTYPE html><html lang="en">' > docs/errors.html
+printf '<!DOCTYPE html>\n<html lang="en">\n' > docs/errors.html
 cat docs/head.html | sed 's/<title>.*<\/title>/<title>List of comptime.ts errors<\/title>/' >> docs/errors.html
+printf '<body>\n<main>\n' >> docs/errors.html
 npx -y marked --gfm < ERRORS.md >> docs/errors.html
+printf '</main>\n' >> docs/errors.html
 cat docs/tail.html >> docs/errors.html
-echo '</html>' >> docs/errors.html
+printf '</body>\n</html>\n' >> docs/errors.html
