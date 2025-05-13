9f18191da2aa34cbe5bd8b5f4fd717c234426e34
Muthu Kumar <muthukumar@thefeathers.in>
2025-05-13 17:25:08 +0530

    ci: fix bun not available in ci

--- docs/build.sh
+++ docs/build.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 
 echo '<!DOCTYPE html><html lang="en">' > docs/index.html
 cat docs/head.html >> docs/index.html
-bun run marked --gfm < README.md | sed 's/ERRORS\.md/\.\/errors/' >> docs/index.html
+npx -y marked --gfm < README.md | sed 's/ERRORS\.md/\.\/errors/' >> docs/index.html
 cat docs/tail.html >> docs/index.html
 echo '</html>' >> docs/index.html
 
 echo '<!DOCTYPE html><html lang="en">' > docs/errors.html
 cat docs/head.html | sed 's/<title>.*<\/title>/<title>List of comptime.ts errors<\/title>/' >> docs/errors.html
-bun run marked --gfm < ERRORS.md >> docs/errors.html
+npx -y marked --gfm < ERRORS.md >> docs/errors.html
 cat docs/tail.html >> docs/errors.html
 echo '</html>' >> docs/errors.html
