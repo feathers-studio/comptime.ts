946dafcb9113d8097a0164f6dc4e11eb5267ca0f
Muthu Kumar <muthukumar@thefeathers.in>
2025-09-11 09:16:48 +0530

    feat: doc builder script with frontmatter support

--- docs/build.ts
+++ docs/build.ts
@@ -1,69 +1,71 @@
-import { spawn as child_process_spawn } from "node:child_process";
-import { createReadStream, createWriteStream, WriteStream } from "node:fs";
+import { createWriteStream, WriteStream } from "node:fs";
 import { readFile } from "node:fs/promises";
-import { platform } from "node:os";
-import { Readable } from "node:stream";
-import { pipeline } from "node:stream/promises";
-
-const spawn = platform() === "win32"
-	? (cmd: string, args: string[]) =>
-		child_process_spawn("cmd.exe", ["/c", cmd, ...args], { stdio: "pipe" })
-	: (cmd: string, args: string[]) =>
-		child_process_spawn(cmd, args, { stdio: "pipe" });
+import { marked } from "marked";
 
 const writeP = (ws: WriteStream, chunk: any) =>
-	new Promise<void>((resolve, reject) =>
-		ws.write(chunk, (err) => err ? reject(err) : resolve())
-	);
-
-const collect = (rs: Readable) =>
-	new Promise<string>((resolve, reject) => {
-		let s = "";
-		const onEnd = () => {
-			rs.off("error", onError);
-			resolve(s);
-		};
-		const onError = (err: Error) => {
-			rs.off("end", onEnd);
-			reject(err);
-		};
-		rs
-			.setEncoding("utf8")
-			.once("error", onError)
-			.once("end", onEnd)
-			.once("data", (chunk) => s += chunk as string);
-	});
+	new Promise<void>((resolve, reject) => ws.write(chunk, err => (err ? reject(err) : resolve())));
 
 const head = await readFile("docs/head.html", "utf8");
+const tail = await readFile("docs/tail.html", "utf8");
 
-const index = createWriteStream("docs/index.html");
+const split = (text: string, delimiter: string) => {
+	const i = text.indexOf(delimiter);
+	if (i === -1) return [text, ""];
+	return [text.slice(0, i), text.slice(i + delimiter.length)];
+};
 
-await writeP(index, '<!DOCTYPE html>\n<html lang="en">\n');
-await writeP(index, head);
-await writeP(index, "<body>\n<main>\n");
-const marked = spawn("npx", ["-y", "marked", "--gfm"]);
-createReadStream("README.md").pipe(marked.stdin);
-const readmeHTML = await collect(marked.stdout);
-await writeP(index, readmeHTML.replaceAll("ERRORS.md", "./errors"));
-await writeP(index, "</main>\n");
-await pipeline(createReadStream("docs/tail.html"), index, { end: false });
-await writeP(index, "</body>\n</html>\n");
-index.end();
+function parseFrontMatter(source: string): { frontmatter: Record<string, string>; markdown: string } {
+	if (!source.startsWith("---\n")) return { frontmatter: {}, markdown: source };
+	const [frontmatter, markdown] = split(source.slice(4), "---");
+	return {
+		frontmatter: frontmatter
+			.trim()
+			.split("\n")
+			.reduce((acc, line) => {
+				const [key, value] = split(line, ":");
+				acc[key.trim()] = value.trim();
+				return acc;
+			}, {} as Record<string, string>),
+		markdown: markdown.trim(),
+	};
+}
 
-const errors = createWriteStream("docs/errors.html");
-await writeP(errors, '<!DOCTYPE html>\n<html lang="en">\n');
-await writeP(
-	errors,
-	head.replace(
-		/<title>.*<\/title>/g,
-		"<title>List of comptime.ts errors</title>",
-	),
-);
-await writeP(errors, "<body>\n<main>\n");
-const markedErrors = spawn("npx", ["-y", "marked", "--gfm"]);
-createReadStream("ERRORS.md").pipe(markedErrors.stdin);
-await pipeline(markedErrors.stdout, errors, { end: false });
-await writeP(errors, "</main>\n");
-await pipeline(createReadStream("docs/tail.html"), errors, { end: false });
-await writeP(errors, "</body>\n</html>\n");
-errors.end();
+const README = {
+	title: "comptime.ts — compile-time expressions for TypeScript",
+	description: "A simple-to-use compiler, Vite, and Bun plugin to evaluate TypeScript expressions at compile time.",
+	featuredImage: "https://comptime.js.org/comptime.ts.jpg",
+	out: "docs/index.html",
+};
+
+async function render(source: string) {
+	const sourcefile = await readFile(source, "utf8");
+
+	const { frontmatter, markdown } = parseFrontMatter(sourcefile);
+	const title = frontmatter.title || README.title;
+	const description = frontmatter.description || README.description;
+	const featuredImage = frontmatter["featured-image"] || README.featuredImage;
+	const out = source === "README.md" ? README.out : frontmatter.out;
+	if (!out) throw new Error(`No output file specified for ${source}`);
+
+	const html = marked.parse(markdown, { async: false });
+
+	const index = createWriteStream(out);
+	await writeP(index, '<!DOCTYPE html>\n<html lang="en">\n');
+	await writeP(
+		index,
+		head
+			.replaceAll("{{TITLE}}", title)
+			.replaceAll("{{DESCRIPTION}}", description)
+			.replaceAll("{{FEATURED_IMAGE}}", featuredImage),
+	);
+	await writeP(index, "<body>\n<main>\n");
+	await writeP(index, html.replaceAll("ERRORS.md", "./errors").replaceAll("SERIALISATION.md", "./serialisation"));
+	await writeP(index, "</main>\n");
+	await writeP(index, tail);
+	await writeP(index, "</body>\n</html>\n");
+	index.end();
+}
+
+await render("README.md");
+await render("ERRORS.md");
+await render("SERIALISATION.md");
